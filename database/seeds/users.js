2633e31c5a8ef012e3017378d5d54883edcb9518
Babatunde <babatundea15@gmail.com>
2020-07-01 15:35:02 +0100

    created seeds and migrations

--- database/seeds/users.js
+++ database/seeds/users.js
@@ -1,13 +1,26 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex("users")
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("users").insert([
+        {
+          id: 1,
+          email: "babatundea15@gmail.com",
+          first_name: "Babatunde",
+          last_name: "Adeniran",
+          password: "nopassword",
+          is_admin: false,
+        },
+        {
+          id: 2,
+          email: "olamideoredola@gmail.com",
+          first_name: "Olamide",
+          last_name: "Oredola",
+          password: "nopassword",
+          is_admin: true,
+        },
       ]);
     });
 };
