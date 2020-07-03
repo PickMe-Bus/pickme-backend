a1a051689df8e5e5d508f5853596d33701b36617
Babatunde <babatundea15@gmail.com>
2020-07-03 11:20:02 +0100

    i added middleware for validating users

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcryptjs");
 
 // Users endpoints here 👇👇👇
 // This is the register endpoint
-router.post("/register", (req, res) => {
+router.post("/register", validateUser, (req, res) => {
   const { email, first_name, last_name, password, is_admin } = req.body;
   //   The password has to be hashed
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -30,3 +30,23 @@ router.post("/register", (req, res) => {
       error.status(500).json({ message: error.message, stack: error.stack });
     });
 });
+
+// Middleware for validating user inputs
+function validateUser(req, res, next) {
+  const addedUser = req.body;
+  if (Object.keys(addedUser).length === 0) {
+    res.status(400).json({ message: "Invalid inputs" });
+  } else if (!addedUser.email) {
+    res.status(400).json({ message: "Please enter a valid email" });
+  } else if (!addedUser.first_name) {
+    res.status(400).json({ message: "Please input your first name" });
+  } else if (!addedUser.last_name) {
+    res.status(400).json({ message: "Please input your last name" });
+  } else if (!addedUser.password) {
+    res.status(400).json({ message: "You have not chosen a password" });
+  } else if (!addedUser.is_admin) {
+    res.status(400).json({ message: "Are you an admin?" });
+  } else {
+    next();
+  }
+}
